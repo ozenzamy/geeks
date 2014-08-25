6bc97a2c063fb0ec1009005b0e65c81a252974c9
Zenzamy Othmane <b00t@laposte.net>
2014-08-25 15:24:41 +0100

    Zenzamy Othmane

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router  = express.Router();
+var validator = require('validator');
+var md5 = require('MD5');
+var userModel = require('../models/user'); 
 
 
 /*GET signup Page*/
@@ -8,10 +11,14 @@ router.get('/', function(req, res){
  res.render('signup', {title: "S'inscrire sur Geek"});
 });
 
+
 router.post('/', function(req, res){
- if(req.body){
-  res.send("Les inscription sont arreter pour l'instant");
- }
+ var firstname = (req.body.firstname.length > 2) ? req.body.firstname : false;
+ var lastname = (req.body.lastname.length > 2) ? req.body.lastname : false;
+ var sexe = req.body.sexe;
+ var email = (validator.isEmail(req.body.email)) ? req.body.email : false; 
+ var password = (req.body.password.length >= 6) ? md5(req.body.password) : false;
+ 
 });
 
 module.exports = router;
